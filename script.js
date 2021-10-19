0357f572b8db36499475a93fcd80e371744f0c46
Luan Costa Sena <88979382+Lu4nSen4@users.noreply.github.com>
2021-10-19 18:50:57 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-var listaDeFilmes = ["https://br.web.img3.acsta.net/pictures/210/527/21052756_20131024195513383.jpg", 
+let listaDeFilmes = []
+  listaDeFilmes['capa dos filmes'] = ["https://br.web.img3.acsta.net/pictures/210/527/21052756_20131024195513383.jpg", 
                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSMO8GOZ_nPw7mInfcuC_1xZ6K4E9i_vsZHQ&usqp=CAU", 
                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlD4uZrCDkW6KLe9ogapOT4JLTklL_gSgn2w&usqp=CAU", 
                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScFVt4sihOWpIzG9cHDkIPjBJRz3Cd2Z9E9w&usqp=CAU", 
                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGxu2mqwgwDdONREgacYghDjq2Z1lFuLrRXw&usqp=CAU", 
                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTadzCV99fwo0sSNdIB01ufEWn1OIWHWzX_TQ&usqp=CAU",]
+                    
+                    
+   listaDeFilmes['link dos filmes'] = ["https://animesonline.club/filme/a-viagem-de-chihiro-online", 
+"https://www.listadeanimes.com/3746.html", 
+"https://animefire.net/animes/tonari-no-totoro-dublado/1", 
+"https://animesonline.club/filme/princesa-mononoke-online", 
+"https://animesonline.club/filme/sussuros-do-coracao-mimi-wo-sumaseba-online", 
+"https://animesonline.club/filme/o-castelo-animado-online", 
+"https://animesonehd.xyz/4747/", 
+"https://animesonehd.xyz/34626/"]
+                     
 
 listaDeFilmes.push("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCADlbQD4LwQF9Q2ubxi_xfRkg-jYNf7TbvQ&usqp=CAU")
 listaDeFilmes.push("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMXWlteODuTxEILzW5cDc5mHAw96Z4eGIt5A&usqp=CAU")
+listaDeFilmes.unshift("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoDBAXfo3GIlWzAoq29N5rFkTOH9HF3vHtuQ&usqp=CAU")
 
 
 
-var linksFilmes = ["https://animesonline.club/filme/a-viagem-de-chihiro-online", 
+/*let linksFilmes = ["https://animesonline.club/filme/a-viagem-de-chihiro-online", 
 "https://www.listadeanimes.com/3746.html", 
 "https://animefire.net/animes/tonari-no-totoro-dublado/1", 
 "https://animesonline.club/filme/princesa-mononoke-online", 
@@ -18,14 +31,14 @@ var linksFilmes = ["https://animesonline.club/filme/a-viagem-de-chihiro-online",
 "https://animesonline.club/filme/o-castelo-animado-online", 
 "https://animesonehd.xyz/4747/", 
 "https://animesonehd.xyz/34626/"
-]
+]*/
 
 
 
 
-for (var indice = 0; indice < listaDeFilmes.length; indice++) {
-  document.write("<a target=_blank href=" + linksFilmes[indice] + 
-  "><img src=" + listaDeFilmes[indice] + ">" + "</a>"); }
+for (let indice = 0; indice < listaDeFilmes.length; indice++) {
+  document.write("<a target=_blank href=" + listaDeFilmes['link dos filmes'] + 
+  "><img src=" + listaDeFilmes['capa dos filmes'] + ">" + "</a>"); }
 
 
 
@@ -34,28 +47,6 @@ for (var indice = 0; indice < listaDeFilmes.length; indice++) {
 
 
 
-let sidebar = document.querySelector(".sidebar");
-  let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
-
-  closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-  });
-
-  searchBtn.addEventListener("click", ()=>{ 
-    sidebar.classList.toggle("open");
-    menuBtnChange(); 
-  });
-
-  
-  function menuBtnChange() {
-   if(sidebar.classList.contains("open")){
-     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-   }else {
-     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
-   }
-  }
 
 
 
